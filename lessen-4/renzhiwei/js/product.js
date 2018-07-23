59af03f4f9eed72e97bf9ac5b564cae3e5d96946
webLion200 <794358525@qq.com>
2018-07-23 16:34:38 +0800

    三

---
+++ lessen-4/renzhiwei/js/product.js
@@ -0,0 +1,11 @@
+$(function(){
+    // new TriggerProdPop({
+    //     trigger_btn: '#prodNamesBtn'
+    // });
+    // new TriggerProdPop({
+    //     trigger_btn: '#comNamesBtn'
+    // });
+    new TriggerProdPop({
+        trigger_btn: '#cloNamesBtn'
+    });
+});
