d8b0f378332bce624acc3692da50c22c274fd31a
Mr-yuan12138 <1596025235@qq.com>
2018-07-25 18:04:34 +0800

    gaozhiyuan

---
+++ lessen-4/gaozhiyuan/js/clothing.js
@@ -0,0 +1,6 @@
+$(function(){     
+    var startInit = new Start({
+       inputElement:'js-clothing-name',
+       moduleNum:'3' 
+    });
+});
---
+++ lessen-4/gaozhiyuan/js/product.js
@@ -0,0 +1,6 @@
+$(function(){     
+    var startInit = new Start({
+       inputElement:'js-prod-name',
+       moduleNum:'1'
+    });
+});
